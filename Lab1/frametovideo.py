463a51e7d04c7b07bf89373f74751110668a50e7
Thom Cruz <42343134+ThomCruz@users.noreply.github.com>
2019-02-24 10:25:51 +0530

    Update frametovideo.py

--- Lab1/frametovideo.py
+++ Lab1/frametovideo.py
@@ -1,48 +1,17 @@
-#!/usr/local/bin/python3
-
 import cv2
-import argparse
 import os
 
-# Construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-ext", "--extension", required=False, default='png', help="extension name. default is 'png'.")
-ap.add_argument("-o", "--output", required=False, default='output.mp4', help="output video file")
-args = vars(ap.parse_args())
+image_folder = 'output'
+video_name = 'video.avi'
 
-# Arguments
-dir_path = '.'
-ext = args['extension']
-output = args['output']
+images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+frame = cv2.imread(os.path.join(image_folder, images[0]))
+height, width, layers = frame.shape
 
-images = []
-for f in os.listdir(dir_path):
-    if f.endswith(ext):
-        images.append(f)
-
-# Determine the width and height from the first image
-image_path = os.path.join(dir_path, images[0])
-frame = cv2.imread(image_path)
-cv2.imshow('video',frame)
-height, width, channels = frame.shape
-
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'mp4v') # Be sure to use lower case
-out = cv2.VideoWriter(output, fourcc, 20.0, (width, height))
+video = cv2.VideoWriter(video_name, 0, 1, (width,height))
 
 for image in images:
+    video.write(cv2.imread(os.path.join(image_folder, image)))
 
-    image_path = os.path.join(dir_path, image)
-    frame = cv2.imread(image_path)
-
-    out.write(frame) # Write out frame to video
-
-    cv2.imshow('video',frame)
-    if (cv2.waitKey(1) & 0xFF) == ord('q'): # Hit `q` to exit
-        break
-
-# Release everything if job is finished
-out.release()
 cv2.destroyAllWindows()
-
-print("The output video is {}".format(output))
+video.release()

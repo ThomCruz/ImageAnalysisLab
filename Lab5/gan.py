930bb991df09c13b146b65b43bd04fd31c95ecf8
Thom Cruz <42343134+ThomCruz@users.noreply.github.com>
2019-04-21 15:03:28 +0530

    Create gan.py

---
+++ Lab5/gan.py
@@ -0,0 +1,343 @@
+'''import numpy as np
+import tensorflow as tf
+def get_y(x):
+    return 10 + x*x
+
+
+def sample_data(n=10000, scale=100):
+    data = []
+
+    x = scale*(np.random.random_sample((n,))-0.5)
+
+    for i in range(n):
+        yi = get_y(x[i])
+        data.append([x[i], yi])
+
+    return np.array(data)
+
+arr = np.zeros(10000, np.uint8)
+arr = sample_data()
+print(arr)
+
+def generator(Z,hsize=[16, 16],reuse=False):
+    with tf.variable_scope("GAN/Generator",reuse=reuse):
+        h1 = tf.layers.dense(Z,hsize[0],activation=tf.nn.leaky_relu)
+        h2 = tf.layers.dense(h1,hsize[1],activation=tf.nn.leaky_relu)
+        out = tf.layers.dense(h2,2)
+
+    return out
+
+def discriminator(X,hsize=[16, 16],reuse=False):
+    with tf.variable_scope("GAN/Discriminator",reuse=reuse):
+        h1 = tf.layers.dense(X,hsize[0],activation=tf.nn.leaky_relu)
+        h2 = tf.layers.dense(h1,hsize[1],activation=tf.nn.leaky_relu)
+        h3 = tf.layers.dense(h2,2)
+        out = tf.layers.dense(h3,1)
+
+    return out, h3
+
+X = tf.placeholder(tf.float32,[None,2])
+Z = tf.placeholder(tf.float32,[None,2])
+
+G_sample = generator(Z)
+r_logits, r_rep = discriminator(X)
+f_logits, g_rep = discriminator(G_sample,reuse=True)
+
+disc_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=r_logits,labels=tf.ones_like(r_logits)) + tf.nn.sigmoid_cross_entropy_with_logits(logits=f_logits,labels=tf.zeros_like(f_logits)))
+gen_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=f_logits,labels=tf.ones_like(f_logits)))
+
+gen_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,scope="GAN/Generator")
+disc_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,scope="GAN/Discriminator")
+
+gen_step = tf.train.RMSPropOptimizer(learning_rate=0.001).minimize(gen_loss,var_list = gen_vars) # G Train step
+disc_step = tf.train.RMSPropOptimizer(learning_rate=0.001).minimize(disc_loss,var_list = disc_vars) # D Train step
+
+batch_size=100000
+for i in range(100001):
+    X_batch = sample_data(n=batch_size)
+    Z_batch = sample_data(batch_size, 2)
+    _, dloss = sess.run([disc_step, disc_loss], feed_dict={X: X_batch, Z: Z_batch})
+    _, gloss = sess.run([gen_step, gen_loss], feed_dict={Z: Z_batch})
+
+    print( "Iterations: %d\t Discriminator loss: %.4f\t Generator loss: %.4f"%(i,dloss,gloss))
+
+import tensorflow as tf
+import numpy as np
+from training_data import *
+import seaborn as sb
+import matplotlib.pyplot as plt
+sb.set()
+
+def sample_Z(m, n):
+    return np.random.uniform(-1., 1., size=[m, n])
+
+def generator(Z,hsize=[16, 16],reuse=False):
+    with tf.variable_scope("GAN/Generator",reuse=reuse):
+        h1 = tf.layers.dense(Z,hsize[0],activation=tf.nn.leaky_relu)
+        h2 = tf.layers.dense(h1,hsize[1],activation=tf.nn.leaky_relu)
+        out = tf.layers.dense(h2,2)
+
+    return out
+
+def discriminator(X,hsize=[16, 16],reuse=False):
+    with tf.variable_scope("GAN/Discriminator",reuse=reuse):
+        h1 = tf.layers.dense(X,hsize[0],activation=tf.nn.leaky_relu)
+        h2 = tf.layers.dense(h1,hsize[1],activation=tf.nn.leaky_relu)
+        h3 = tf.layers.dense(h2,2)
+        out = tf.layers.dense(h3,1)
+
+    return out, h3
+
+
+X = tf.placeholder(tf.float32,[None,2])
+Z = tf.placeholder(tf.float32,[None,2])
+
+G_sample = generator(Z)
+r_logits, r_rep = discriminator(X)
+f_logits, g_rep = discriminator(G_sample,reuse=True)
+
+disc_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=r_logits,labels=tf.ones_like(r_logits)) + tf.nn.sigmoid_cross_entropy_with_logits(logits=f_logits,labels=tf.zeros_like(f_logits)))
+gen_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=f_logits,labels=tf.ones_like(f_logits)))
+
+gen_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,scope="GAN/Generator")
+disc_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,scope="GAN/Discriminator")
+
+gen_step = tf.train.RMSPropOptimizer(learning_rate=0.001).minimize(gen_loss,var_list = gen_vars) # G Train step
+disc_step = tf.train.RMSPropOptimizer(learning_rate=0.001).minimize(disc_loss,var_list = disc_vars) # D Train step
+
+
+
+# sess = tf.Session(config=config)
+sess = tf.Session()
+tf.global_variables_initializer().run(session=sess)
+
+batch_size = 256
+nd_steps = 10
+ng_steps = 10
+
+x_plot = sample_data(n=batch_size)
+
+f = open('loss_logs.csv','w')
+f.write('Iteration,Discriminator Loss,Generator Loss\n')
+
+for i in range(10001):
+    X_batch = sample_data(n=batch_size)
+    Z_batch = sample_Z(batch_size, 2)
+
+    for _ in range(nd_steps):
+        _, dloss = sess.run([disc_step, disc_loss], feed_dict={X: X_batch, Z: Z_batch})
+    rrep_dstep, grep_dstep = sess.run([r_rep, g_rep], feed_dict={X: X_batch, Z: Z_batch})
+
+    for _ in range(ng_steps):
+        _, gloss = sess.run([gen_step, gen_loss], feed_dict={Z: Z_batch})
+
+    rrep_gstep, grep_gstep = sess.run([r_rep, g_rep], feed_dict={X: X_batch, Z: Z_batch})
+
+    print ("Iterations: %d\t Discriminator loss: %.4f\t Generator loss: %.4f"%(i,dloss,gloss))
+    if i%10 == 0:
+        f.write("%d,%f,%f\n"%(i,dloss,gloss))
+
+    if i%1000 == 0:
+        plt.figure()
+        g_plot = sess.run(G_sample, feed_dict={Z: Z_batch})
+        xax = plt.scatter(x_plot[:,0], x_plot[:,1])
+        gax = plt.scatter(g_plot[:,0],g_plot[:,1])
+
+        plt.legend((xax,gax), ("Real Data","Generated Data"))
+        plt.title('Samples at Iteration %d'%i)
+        plt.tight_layout()
+        plt.savefig('../plots/iterations/iteration_%d.png'%i)
+        plt.close()
+
+        plt.figure()
+        rrd = plt.scatter(rrep_dstep[:,0], rrep_dstep[:,1], alpha=0.5)
+        rrg = plt.scatter(rrep_gstep[:,0], rrep_gstep[:,1], alpha=0.5)
+        grd = plt.scatter(grep_dstep[:,0], grep_dstep[:,1], alpha=0.5)
+        grg = plt.scatter(grep_gstep[:,0], grep_gstep[:,1], alpha=0.5)
+
+
+        plt.legend((rrd, rrg, grd, grg), ("Real Data Before G step","Real Data After G step",
+                               "Generated Data Before G step","Generated Data After G step"))
+        plt.title('Transformed Features at Iteration %d'%i)
+        plt.tight_layout()
+        plt.savefig('../plots/features/feature_transform_%d.png'%i)
+        plt.close()
+
+        plt.figure()
+
+        rrdc = plt.scatter(np.mean(rrep_dstep[:,0]), np.mean(rrep_dstep[:,1]),s=100, alpha=0.5)
+        rrgc = plt.scatter(np.mean(rrep_gstep[:,0]), np.mean(rrep_gstep[:,1]),s=100, alpha=0.5)
+        grdc = plt.scatter(np.mean(grep_dstep[:,0]), np.mean(grep_dstep[:,1]),s=100, alpha=0.5)
+        grgc = plt.scatter(np.mean(grep_gstep[:,0]), np.mean(grep_gstep[:,1]),s=100, alpha=0.5)
+
+        plt.legend((rrdc, rrgc, grdc, grgc), ("Real Data Before G step","Real Data After G step",
+                               "Generated Data Before G step","Generated Data After G step"))
+
+        plt.title('Centroid of Transformed Features at Iteration %d'%i)
+        plt.tight_layout()
+        plt.savefig('../plots/features/feature_transform_centroid_%d.png'%i)
+        plt.close()
+
+f.close()'''
+from __future__ import print_function, division
+
+from keras.datasets import mnist
+from keras.layers import Input, Dense, Reshape, Flatten, Dropout
+from keras.layers import BatchNormalization, Activation, ZeroPadding2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model
+from keras.optimizers import Adam
+
+import matplotlib.pyplot as plt
+
+import sys
+
+import numpy as np
+
+class GAN():
+    def __init__(self):
+        self.img_rows = 28
+        self.img_cols = 28
+        self.channels = 1
+        self.img_shape = (self.img_rows, self.img_cols, self.channels)
+        self.latent_dim = 100
+
+        optimizer = Adam(0.0002, 0.5)
+
+        # Build and compile the discriminator
+        self.discriminator = self.build_discriminator()
+        self.discriminator.compile(loss='binary_crossentropy',
+            optimizer=optimizer,
+            metrics=['accuracy'])
+
+        # Build the generator
+        self.generator = self.build_generator()
+
+        # The generator takes noise as input and generates imgs
+        z = Input(shape=(self.latent_dim,))
+        img = self.generator(z)
+
+        # For the combined model we will only train the generator
+        self.discriminator.trainable = False
+
+        # The discriminator takes generated images as input and determines validity
+        validity = self.discriminator(img)
+
+        # The combined model  (stacked generator and discriminator)
+        # Trains the generator to fool the discriminator
+        self.combined = Model(z, validity)
+        self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
+
+
+    def build_generator(self):
+
+        model = Sequential()
+
+        model.add(Dense(256, input_dim=self.latent_dim))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(512))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(1024))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(np.prod(self.img_shape), activation='tanh'))
+        model.add(Reshape(self.img_shape))
+
+        model.summary()
+
+        noise = Input(shape=(self.latent_dim,))
+        img = model(noise)
+
+        return Model(noise, img)
+
+    def build_discriminator(self):
+
+        model = Sequential()
+
+        model.add(Flatten(input_shape=self.img_shape))
+        model.add(Dense(512))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dense(256))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dense(1, activation='sigmoid'))
+        model.summary()
+
+        img = Input(shape=self.img_shape)
+        validity = model(img)
+
+        return Model(img, validity)
+
+    def train(self, epochs, batch_size=128, sample_interval=50):
+
+        # Load the dataset
+        (X_train, _), (_, _) = mnist.load_data()
+
+        # Rescale -1 to 1
+        X_train = X_train / 127.5 - 1.
+        X_train = np.expand_dims(X_train, axis=3)
+
+        # Adversarial ground truths
+        valid = np.ones((batch_size, 1))
+        fake = np.zeros((batch_size, 1))
+
+        for epoch in range(epochs):
+
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
+
+            # Select a random batch of images
+            idx = np.random.randint(0, X_train.shape[0], batch_size)
+            imgs = X_train[idx]
+
+            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
+
+            # Generate a batch of new images
+            gen_imgs = self.generator.predict(noise)
+
+            # Train the discriminator
+            d_loss_real = self.discriminator.train_on_batch(imgs, valid)
+            d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
+            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+            # ---------------------
+            #  Train Generator
+            # ---------------------
+
+            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
+
+            # Train the generator (to have the discriminator label samples as valid)
+            g_loss = self.combined.train_on_batch(noise, valid)
+
+            # Plot the progress
+            print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
+
+            # If at save interval => save generated image samples
+            if epoch % sample_interval == 0:
+                self.sample_images(epoch)
+
+    def sample_images(self, epoch):
+        r, c = 5, 5
+        noise = np.random.normal(0, 1, (r * c, self.latent_dim))
+        gen_imgs = self.generator.predict(noise)
+
+        # Rescale images 0 - 1
+        gen_imgs = 0.5 * gen_imgs + 0.5
+
+        fig, axs = plt.subplots(r, c)
+        cnt = 0
+        for i in range(r):
+            for j in range(c):
+                axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
+                axs[i,j].axis('off')
+                cnt += 1
+        fig.savefig("images/%d.png" % epoch)
+        plt.close()
+
+
+if __name__ == '__main__':
+    gan = GAN()
+    gan.train(epochs=30000, batch_size=32, sample_interval=200)
